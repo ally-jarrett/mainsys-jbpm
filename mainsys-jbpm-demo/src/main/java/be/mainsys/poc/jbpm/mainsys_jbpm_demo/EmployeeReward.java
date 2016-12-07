c8a9d06ab2704e255a47cca7e3b590c2030442b3
Ally Jarrett <ajarrett@redhat.com>
2016-12-07 14:26:49 +0100

    Changes

--- mainsys-jbpm-demo/src/main/java/be/mainsys/poc/jbpm/mainsys_jbpm_demo/EmployeeReward.java
+++ mainsys-jbpm-demo/src/main/java/be/mainsys/poc/jbpm/mainsys_jbpm_demo/EmployeeReward.java
@@ -12,59 +12,69 @@ public class EmployeeReward implements Serializable {
 	}
 
 	private static final long serialVersionUID = 1L;
-	
-	private String recipient; 
+
+	private String recipient;
 	private String rewardReason;
 	private Integer rewardAmount;
 	private String rewardManagerComment;
-	private boolean rewardManagerApproval; ;
+	private boolean rewardManagerApproval;;
 	private boolean rewardHRApproval;
-	
+
 	public String getRewardReason() {
 		return rewardReason;
 	}
+
 	public void setRewardReason(String rewardReason) {
 		this.rewardReason = rewardReason;
 	}
+
 	public Integer getRewardAmount() {
 		return rewardAmount;
 	}
+
 	public void setRewardAmount(Integer rewardAmount) {
 		this.rewardAmount = rewardAmount;
 	}
+
 	public String getRewardManagerComment() {
 		return rewardManagerComment;
 	}
+
 	public void setRewardManagerComment(String rewardManagerComment) {
 		this.rewardManagerComment = rewardManagerComment;
 	}
+
 	public boolean getRewardManagerApproval() {
 		return rewardManagerApproval;
 	}
+
 	public void setRewardManagerApproval(boolean rewardManagerApproval) {
 		this.rewardManagerApproval = rewardManagerApproval;
 	}
+
 	public boolean getRewardHRApproval() {
 		return rewardHRApproval;
 	}
+
 	public void setRewardHRApproval(boolean rewardHRApproval) {
 		this.rewardHRApproval = rewardHRApproval;
 	}
-	
+
 	public String getRecipient() {
 		return recipient;
 	}
+
 	public void setRecipient(String recipient) {
 		this.recipient = recipient;
 	}
-	
-	public EmployeeReward(){
-		
+
+	public EmployeeReward() {
+
 	}
-	
+
 	public EmployeeReward(String recipient, String rewardReason, Integer rewardAmount, String rewardManagerComment,
 			boolean rewardManagerApproval, boolean rewardHRApproval) {
-		this.recipient = recipient; 
+		this.recipient = recipient;
 		this.rewardReason = rewardReason;
 		this.rewardAmount = rewardAmount;
 		this.rewardManagerComment = rewardManagerComment;
